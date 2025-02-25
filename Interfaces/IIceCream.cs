c6337e8e8b4c9aa1908425402372f8fb3dea40d2
tamarP85 <9745544b@gmail.com>
2025-02-25 15:30:52 +0200

    second commit T

---
+++ Interfaces/IIceCream.cs
@@ -0,0 +1,11 @@
+using WebApiProject.Models;
+namespace WebApiProject.Interfaces;
+public interface IIceCreamService
+{
+    List<IceCream> Get();
+    IceCream Get(int id);
+    int Insert(IceCream newIceCream);
+    bool UpDate(int id,IceCream newIceCream);
+    bool Delete(int id);
+       
+}
